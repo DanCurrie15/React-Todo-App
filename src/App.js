13f030364e42843dd5cd606012e551eb1ceab9d4
Dan Currie <danjc_15@hotmail.com>
2019-02-01 20:10:47 -0500

    New component & CSS
    made component for todo item and added css to style app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <input type="checkbox" />
-        <p>Placeholder text here</p>
-
-        <input type="checkbox" />
-        <p>Placeholder text here</p>
-
-        <input type="checkbox" />
-        <p>Placeholder text here</p>
-
-        <input type="checkbox" />
-        <p>Placeholder text here</p>
+      <div className="todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
       </div>
     );
   }
